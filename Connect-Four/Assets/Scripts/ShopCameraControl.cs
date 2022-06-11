d55db4badb3819e67b5a82d7785be8d36f178da3
AshenTales <lukeekul76@gmail.com>
2022-06-10 22:07:04 -0600

    Simplified the ShopCameraControl.cs script & added the script to the main camera for test/debug
    I removed the reference to Transform cameraPos as it is unneeded due to a transform.position call. I also added a buffer var for adjustability in future.

--- Connect-Four/Assets/Scripts/ShopCameraControl.cs
+++ Connect-Four/Assets/Scripts/ShopCameraControl.cs
@@ -5,28 +5,27 @@ using UnityEngine;
 public class ShopCameraControl : MonoBehaviour
 {
     [SerializeField] private int[] minMax = {0,22};
-    [SerializeField] private float scrollSpeed =200;
+    [SerializeField] private float scrollSpeed =20;
+    [SerializeField] private float scrollBuffer = 0.25f; // Buffer around the edges of the screen from 0-1 as a fraction of the screen.
     private float scrollX;
 
-    private Transform cameraPos;
     void Start()
     {
-        cameraPos = gameObject.GetComponent<Transform>();
     }
 
     void Update()
     {
-        if (Input.mousePosition.x / Screen.width < 0.2f) {
+        if (Input.mousePosition.x / Screen.width < scrollBuffer) {
 
             scrollX -= scrollSpeed* Time.deltaTime;
 
         }
-        else if (Input.mousePosition.x / Screen.width > 0.8f)
+        else if (Input.mousePosition.x / Screen.width > 1 - scrollBuffer)
         {
 
             scrollX += scrollSpeed * Time.deltaTime;
 
         }
-        cameraPos.position = new Vector3 (Mathf.Clamp(scrollX, minMax[0], minMax[1]), 0 , -10);
+        transform.position = new Vector3(Mathf.Clamp(scrollX, minMax[0], minMax[1]), 0, -10);
     }
 }
